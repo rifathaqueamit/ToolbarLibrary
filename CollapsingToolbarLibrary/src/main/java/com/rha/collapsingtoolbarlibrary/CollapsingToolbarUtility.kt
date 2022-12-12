f3567d52b02757428694e13e2ae5ac30358201b1
Rifat Haque Amit <rifat.haque@samsung.com>
2022-12-12 16:04:33 +0600

    Added example

--- CollapsingToolbarLibrary/src/main/java/com/rha/collapsingtoolbarlibrary/CollapsingToolbarUtility.kt
+++
@@ -1,7 +0,0 @@
-package com.rha.collapsingtoolbarlibrary
-
-object CollapsingToolbarUtility {
-    fun getTitle(): String {
-        return "Abcd"
-    }
-}
--- app/src/main/java/com/rha/toolbarlibrary/MainActivity.kt
+++ app/src/main/java/com/rha/toolbarlibrary/MainActivity.kt
@@ -2,13 +2,10 @@ package com.rha.toolbarlibrary
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import com.rha.collapsingtoolbarlibrary.CollapsingToolbarUtility
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-
-        val title = CollapsingToolbarUtility.getTitle()
     }
 }
